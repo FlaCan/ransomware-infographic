e7a38f274675f80091b2417711bc223a87e707c5
flavio@google.com <flavio.cantanna@gmail.com>
2021-10-20 08:24:35 +0100

    No changes, restars from tower

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ gsap.set("#countdown > path", {transformOrigin: "50% 50%"})
 
 /* Animations *********************************************************************************************/
 
-gsap.fromTo(".drop", {y: -30}, {duration: 0.75, stagger:0.25, y: 0, repeat: -1, yoyo: true})
+const animDrop = gsap.fromTo(".drop", {y: -30}, {duration: 0.75, stagger:0.25, y: 0, repeat: -1, yoyo: true})
 gsap.to("#countdown > path", {duration: 2, rotate:360, repeat: -1, ease: "linear"}, "<")
-const pop = gsap.to(".countdown#countdown_backup-offline", {paused: true, autoAlpha:1, scale: 1.5, ease: "back"}, "<")
+const animClock = gsap.to(".countdown#countdown_backup-offline", {paused: true, autoAlpha:1, scale: 1.5, ease: "back"}, "<")
 
 
 /* Barriers */
 
 const animBarrierL1 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.25}})
-    .to(".barrier-L1 .barrier-line", {scaleX: 0}, "<")
-    .to(".barrier-L1 .barrier-plus", {scaleY: 1}, "<")
+    .to("#barrier-L1 .barrier-line", {scaleX: 0}, "<")
+    .to("#barrier-L1 .barrier-plus", {scaleY: 1}, "<")
     .to(".slolen-L1", {autoAlpha: 1}, "<")
     .to(".malware-L1", {autoAlpha: 1}, "<")
     .to(".user-L1 .workstation", {fill: "#CC0000"}, "<")
@@ -34,8 +34,8 @@ const animBarrierL1 = gsap.timeline({paused: true, reversed: true, defaults:{dur
     .to("#target-malware-L1-right", {visibility: "visible"}, "<")
     
 const animBarrierL2 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.25}})
-    .to(".barrier-L2 .barrier-line", {scaleX: 0}, "<")
-    .to(".barrier-L2 .barrier-plus", {scaleY: 1}, "<")
+    .to("#barrier-L2 .barrier-line", {scaleX: 0}, "<")
+    .to("#barrier-L2 .barrier-plus", {scaleY: 1}, "<")
     .to(".malware-L2", {autoAlpha: 1}, "<")
     .to(".device-L2", {fill: "#CC0000"}, "<")
     .to(".user-L2 .workstation", {fill: "#CC0000"}, "<")
@@ -47,8 +47,8 @@ const animBarrierL2 = gsap.timeline({paused: true, reversed: true, defaults:{dur
 
     
 const animBarrierL3 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.25}})
-    .to(".barrier-L3 .barrier-line", {scaleX: 0}, "<")
-    .to(".barrier-L3 .barrier-plus", {scaleY: 1}, "<")
+    .to("#barrier-L3 .barrier-line", {scaleX: 0}, "<")
+    .to("#barrier-L3 .barrier-plus", {scaleY: 1}, "<")
     .to(".stolen-L3", {autoAlpha: 1}, "<")
     .to(".malware-L3", {autoAlpha: 1}, "<" )
     .to(".device-L3", {fill: "#CC0000"}, "<")
@@ -62,8 +62,8 @@ const animBarrierL3 = gsap.timeline({paused: true, reversed: true, defaults:{dur
     
     
 const animBarrierL4 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.25}})
-    .to(".barrier-L4 .barrier-line", {scaleX: 0}, "<")
-    .to(".barrier-L4 .barrier-plus", {scaleY: 1}, "<")
+    .to("#barrier-L4 .barrier-line", {scaleX: 0}, "<")
+    .to("#barrier-L4 .barrier-plus", {scaleY: 1}, "<")
     .to(".malware-L4", {autoAlpha: 1}, "<" )
     .to(".device-L4", {fill: "#CC0000"}, "<")
     .to("#target-malware-L4", {visibility: "visible"}, "<")
@@ -79,51 +79,56 @@ const targetClickBarrierL4 = select("#target-click-barrier-L4")
 
 targetClickBarrierL1.addEventListener("click", () => {
     if (animBarrierL1.reversed()) {
+        animDrop.pause()
         animBarrierL1.play() // sets self to false (open)
     } else {
+        animDrop.play()
         animBarrierL1.reverse() // sets self to true (closed)
         animBarrierL2.reverse() // sets G2 to true (closed)
         animBarrierL3.reverse() // sets G3 to true (closed)
-        pop.reverse()
+        animClock.reverse()
         animBarrierL4.reverse() // sets G4 to true (closed)
     }
 }, false)
 
 targetClickBarrierL2.addEventListener("click", () => {
     if (animBarrierL2.reversed()) {
+        animDrop.pause()
         animBarrierL1.play(); //sets G1 to false (open)
         animBarrierL2.play() //sets self to false (open)
     } else {
         animBarrierL2.reverse() //sets self to true (closed)
         animBarrierL3.reverse() // sets G3 to true (closed)
-        pop.reverse()
+        animClock.reverse()
         animBarrierL4.reverse() // sets G4 to true (closed)
     }
 }, false)
 
 targetClickBarrierL3.addEventListener("click", () => {
     if (animBarrierL3.reversed()) {
+        animDrop.pause()
         animBarrierL1.play() //sets G1 to false (open)
         animBarrierL2.play() //sets G2 to false (open)
         animBarrierL3.play() //sets self to false (open)
-        pop.play()
+        animClock.play()
 
     } else {
         animBarrierL3.reverse() // sets self to true (closed)
-        pop.reverse()
+        animClock.reverse()
         animBarrierL4.reverse() // sets G4 to true (closed)                
     }
 }, false)
 
 targetClickBarrierL4.addEventListener("click", () => {
     if (animBarrierL4.reversed()) {
+        animDrop.pause()
         animBarrierL1.play() //sets G1 to false (open)
         animBarrierL2.play() //sets G2 to false (open)
         animBarrierL3.play() //sets G3 to false (open)
-        pop.reverse()
+        animClock.reverse()
         animBarrierL4.play() //sets self to false (open)
     } else {
-        pop.play()
+        animClock.play()
         animBarrierL4.reverse() // sets self to true (closed)
     }
 }, false)
