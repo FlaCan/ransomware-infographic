0090f1a1cb9b23fa5a2ceeb731575f857c1640d9
flavio@google.com <flavio.cantanna@gmail.com>
2021-11-01 17:56:04 +0000

    Replaces barriers with open from center - Introduces  an issue: barrier speeches are below other objects

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const select = (el) => document.querySelector(el)
 const selectAll = (el) => document.querySelectorAll(el)
 
-const targetClickL1 = select("#target-click-barrier-L1")
-const targetClickL2 = select("#target-click-barrier-L2")
-const targetClickL3 = select("#target-click-barrier-L3")
-const targetClickL4 = select("#target-click-barrier-L4")
+
 
 /* Initial states ****************************************************************************************/
 
-gsap.set(".barrier-line", {scaleY: 1, transformOrigin: "50% 50%"})
-gsap.set(".barrier-plus", {scaleY: 0, transformOrigin: "50% 50%"})
+
+gsap.set(".line-left", {transformOrigin: "0% 50%"})
+gsap.set(".line-right", {transformOrigin: "100% 50%"})
 gsap.set("#countdown > path", {transformOrigin: "50% 50%"})
 
 
@@ -21,11 +19,22 @@ gsap.to("#countdown > path", {duration: 2, rotate:360, repeat: -1, ease: "linear
 const animClock = gsap.to(".countdown#countdown_backup-offline", {paused: true, autoAlpha:1, scale: 1.5, ease: "back"}, "<")
 
 
+function animBarrier(el) {
+    const tl = gsap.timeline()
+    .to(el + " .line-left", {scaleX: 0}, "<")
+    .to(el + " .line-right", {scaleX: 0}, "<")
+    .to(el + " .button-left", {x: -600}, "<")
+    .to(el + " .button-right", {x: 600}, "<")
+    .to(el + " .click-left", {x: -600}, "<")
+    .to(el + " .click-right", {x: 600}, "<")
+    .to(el + " .barrier-line-muted", {autoAlpha: 1}, "<")
+    return tl
+}
+
 /* Barriers */
 
 const animBarrierL1 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.2}})
-    .to("#barrier-L1 .barrier-line", {scaleX: 0}, "<")
-    .to("#barrier-L1 .barrier-plus", {scaleY: 1}, "<")
+    .add(animBarrier("#barrier-L1"))
     .to(".malware-L1", {autoAlpha: 1}, "+=0.1")
     .to(".slolen-L1", {autoAlpha: 1}, "<")
     .to(".user-L1 .workstation", {fill: "#CC0000"}, "<")
@@ -36,9 +45,9 @@ const animBarrierL1 = gsap.timeline({paused: true, reversed: true, defaults:{dur
     .to("#target-malware-L1-left", {visibility: "visible"}, "<")
     .to("#target-malware-L1-right", {visibility: "visible"}, "<")
     
+
 const animBarrierL2 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.2}})
-    .to("#barrier-L2 .barrier-line", {scaleX: 0}, "<")
-    .to("#barrier-L2 .barrier-plus", {scaleY: 1}, "<")
+    .add(animBarrier("#barrier-L2"))
     .to(".malware-L2", {autoAlpha: 1}, "+=0.1")
     .to(".slolen-L2", {autoAlpha: 1}, "<")
     .to(".device-L2", {fill: "#CC0000"}, "<")
@@ -51,8 +60,7 @@ const animBarrierL2 = gsap.timeline({paused: true, reversed: true, defaults:{dur
 
     
 const animBarrierL3 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.2}})
-    .to("#barrier-L3 .barrier-line", {scaleX: 0}, "<")
-    .to("#barrier-L3 .barrier-plus", {scaleY: 1}, "<")
+    .add(animBarrier("#barrier-L3"))
     .to(".malware-L3", {autoAlpha: 1}, "+=0.1" )
     .to(".stolen-L3", {autoAlpha: 1}, "<")    
     .to(".device-L3", {fill: "#CC0000"}, "<")
@@ -66,8 +74,7 @@ const animBarrierL3 = gsap.timeline({paused: true, reversed: true, defaults:{dur
     
     
 const animBarrierL4 = gsap.timeline({paused: true, reversed: true, defaults:{duration:0.2}})
-    .to("#barrier-L4 .barrier-line", {scaleX: 0}, "<")
-    .to("#barrier-L4 .barrier-plus", {scaleY: 1}, "<")
+    .add(animBarrier("#barrier-L4"))
     .to(".malware-L4", {autoAlpha: 1}, "+=0.1" )
     .to(".device-L4", {fill: "#CC0000"}, "<")
     .to("#target-malware-L4", {visibility: "visible"}, "<")
@@ -76,63 +83,93 @@ const animBarrierL4 = gsap.timeline({paused: true, reversed: true, defaults:{dur
 /* Click Listeners **********************************************************************************************/
 
 
+const clicksL1 = selectAll("#barrier-L1 .click")
+const clicksL2 = selectAll("#barrier-L2 .click")
+const clicksL3 = selectAll("#barrier-L3 .click")
+const clicksL4 = selectAll("#barrier-L4 .click")
+
+clicksL1.forEach(item => {
+
+    item.addEventListener("click", () => {
+        if (animBarrierL1.reversed()) {
+            animDrop.pause()
+            animBarrierL1.play() 
+        } else {
+            animDrop.play()
+            animBarrierL1.reverse() 
+            animBarrierL2.reverse() 
+            animBarrierL3.reverse() 
+            animBarrierL4.reverse() 
+            animClock.reverse()
+        }
+    }, false)
+    
+});
 
 
-targetClickL1.addEventListener("click", () => {
-    if (animBarrierL1.reversed()) {
-        animDrop.pause()
-        animBarrierL1.play() 
-    } else {
-        animDrop.play()
-        animBarrierL1.reverse() 
-        animBarrierL2.reverse() 
-        animBarrierL3.reverse() 
-        animBarrierL4.reverse() 
-        animClock.reverse()
-    }
-}, false)
+clicksL2.forEach(item => {
+    
+    item.addEventListener("click", () => {
+        if (animBarrierL2.reversed()) {
+            animDrop.pause()
+            animBarrierL1.play()
+            animBarrierL2.play()
+        } else {
+            animBarrierL2.reverse()
+            animBarrierL3.reverse()
+            animBarrierL4.reverse()
+            animClock.reverse()
+        }
+    }, false)
 
-targetClickL2.addEventListener("click", () => {
-    if (animBarrierL2.reversed()) {
-        animDrop.pause()
-        animBarrierL1.play()
-        animBarrierL2.play()
-    } else {
-        animBarrierL2.reverse()
-        animBarrierL3.reverse()
-        animBarrierL4.reverse()
-        animClock.reverse()
-    }
-}, false)
+});
 
-targetClickL3.addEventListener("click", () => {
-    if (animBarrierL3.reversed()) {
-        animDrop.pause()
-        animBarrierL1.play()
-        animBarrierL2.play()
-        animBarrierL3.play()
-        animClock.play()
 
-    } else {
-        animBarrierL3.reverse()
-        animBarrierL4.reverse()
-        animClock.reverse()
-    }
-}, false)
+clicksL3.forEach(item => {
 
-targetClickL4.addEventListener("click", () => {
-    if (animBarrierL4.reversed()) {
-        animDrop.pause()
-        animBarrierL1.play()
-        animBarrierL2.play()
-        animBarrierL3.play()
-        animBarrierL4.play()
-        animClock.reverse()
-    } else {
-        animBarrierL4.reverse()
-        animClock.play()
-    }
-}, false)
+    item.addEventListener("click", () => {
+        if (animBarrierL3.reversed()) {
+            animDrop.pause()
+            animBarrierL1.play()
+            animBarrierL2.play()
+            animBarrierL3.play()
+            animClock.play()
+    
+        } else {
+            animBarrierL3.reverse()
+            animBarrierL4.reverse()
+            animClock.reverse()
+        }
+    }, false)
+
+});
+
+
+
+
+
+
+clicksL4.forEach(item => {
+
+    item.addEventListener("click", () => {
+        if (animBarrierL4.reversed()) {
+            animDrop.pause()
+            animBarrierL1.play()
+            animBarrierL2.play()
+            animBarrierL3.play()
+            animBarrierL4.play()
+            animClock.reverse()
+        } else {
+            animBarrierL4.reverse()
+            animClock.play()
+        }
+    }, false)
+    
+});
+
+
+
+
 
 
 /* Hover listeners - Rely on current html structure *********************************************************/
